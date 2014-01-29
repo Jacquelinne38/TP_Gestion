feea66c9b34278353ca9705618eeceb757774155
guda <yannick.luzy@gmail.com>
2014-01-29 11:58:28 +0100

    add test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,25 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	render();
     }
 
+    public static void index2() {
+		List tasks = Task.find("order by id desc").fetch();
+    	render(tasks);
+	}
+
+    public static void createTask(String title) {
+    	Task task = new Task(title).save();
+    	renderJSON(task);
+    }
+
+    public static void changeStatut(Long id, boolean checked) {
+    	Task task = Task.findById(id);
+    	task.checked = checked;
+    	task.save();
+    	renderJSON(task);
+
+    }
+    
 }
